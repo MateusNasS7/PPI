73612295c65c6a39be3c92aac601d9d662967782
Mateus Nascimento Silveira <wonemns@gmail.com>
2024-12-05 08:03:57 -0300

    search

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Product from "./Product";
 import { CircularProgress } from "@mui/material";
 import { CartContext } from "../context/CartContext";
@@ -8,24 +8,56 @@ export default function Shop() {
 
     const { products, loading, error } = useContext(CartContext);
 
+    const searchInput = useRef("");
+
+    const [filteredItems, setFilteredItems] = useState([]);
+
+    useEffect(() => {
+        if (products) {
+            setFilteredItems(products);
+        }
+    }, [products]);
+
+    function handleSearch() {
+        const term = searchInput.current.value.toLowerCase();
+        setFilteredItems(
+            products.filter((item) => item.title.toLowerCase().includes(term))
+        );
+    }
+
+    function clearSearch() {
+        searchInput.current.value = "";
+        setFilteredItems(products);
+    }
+
     return (
         <section id="shop">
             <h2>Elegant Products for Everyone</h2>
+            <div className={styles.search_container}>
+                <div className={styles.search_box}>
+                    <input className={styles.search_input} ref={searchInput} type="text" placeholder="Type to Search" onChange={handleSearch} />
+                    <button className={styles.search_clear} onClick={clearSearch}>
+                        CLEAR
+                    </button>
+                </div>
+            </div>
 
             <ul id="products">
                 {error && <p>{error}</p>}
-                {!loading && products ? (
-                    products.map((product) => (
-                        <li key={product.id}>
-                            <Product {...product} />
-                        </li>
-                    ))
-                ) : (
+                {loading &&
                     <div id="loading">
                         <CircularProgress size="10rem" color="inherit" />
                         <p>Loading products...</p>
-                    </div>
-                )}
+                    </div>}
+                    {!loading && !error && filteredItems.length > 0 ? (
+                        filteredItems.map((product) => (
+                            <li key={product.id}>
+                                <Product {...product} />
+                            </li>
+                        ))
+                    ) : (
+                        <p>Not Found!</p>
+                    )}
             </ul>
 
         </section>
